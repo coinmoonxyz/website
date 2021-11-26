f276eeb3f6f939c2efe5755c9d28189e68b681d4
coinmoonxyz <coinmoonxyz@gmail.com>
2021-11-25 23:40:33 -0500

    kofi update

--- src/components/organisms/kofi.js
+++ src/components/organisms/kofi.js
@@ -13,7 +13,7 @@ const StyledKofi = styled.div`
 const Kofi = () => (
   <StyledKofi>
     <p style={{ marginBottom: "1rem" }}>
-      후원금은 블로그 유지에 큰 도움이 됩니다. 감사합니다!
+      후원금은 블로그 유지에 큰 도움이 됩니다. Brave 브라우저 사용하시는 분들은 주소창 오른쪽 삼각형을 누르셔서 팁을 주실 수도 있어요. 감사합니다!
     </p>
     <a href="https://ko-fi.com/V7V74G7ID" target="_blank" rel="noreferrer">
       <img
